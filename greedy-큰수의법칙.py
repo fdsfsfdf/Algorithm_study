7f3568a1646eea8367ce28cdc57ceadab772ba77
fdsfsfdf <32867503+fdsfsfdf@users.noreply.github.com>
2021-08-26 22:47:56 +0900

    Update greedy-큰수의법칙.py

--- greedy-큰수의법칙.py
+++ greedy-큰수의법칙.py
@@ -7,7 +7,7 @@ no = list(map(int,input().split()))
 no.sort(reverse=True)
 
 sum = 0
-cnt = 0
+
 h = True
 t = m
 while h:
@@ -16,11 +16,11 @@ while h:
       h = False
       break
     sum += no[0]
-    t=t-1
-    
+    t=t-1    
   if t>=1:
     sum+=no[1]
     t = t-1
+    
 print(sum)
 
 
